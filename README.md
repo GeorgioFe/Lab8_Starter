5e908fd3ad7543cd04e6f8b883c49e632fe1478e
Georgio Feghali <75293296+GeorgioFe@users.noreply.github.com>
2023-05-28 18:03:44 -0700

    Lab 8 done.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Lab 8 - Starter
+
+__1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.__
+
+Automated tests should be incorporated within a GitHub action that runs whenever code is pushed. This approach, known as continuous integration (CI), provides early feedback, ensures integration with existing code, supports collaboration and code review, and offers scalability and efficiency benefits. Manual testing before pushing code or running tests after development is completed is not as effective in catching issues early and maintaining code integrity.
+
+__2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)__
+
+No
+
+__3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.__
+
+The "message" feature of a messaging application should not be tested using unit tests due to its large-scale nature and the involvement of multiple interacting components. Instead, it is more appropriate to use end-to-end tests for this feature. End-to-end tests simulate the entire user journey from start to finish, providing a comprehensive evaluation of the feature's functionality by emulating real user actions. These tests verify the complete process of writing and sending a message, ensuring that all components and interactions work correctly.
+
+__4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.__
+
+Yes, unit testing is suitable for this small-scale feature since it allows for independent testing without the need to evaluate the interactions of multiple components.
+
+### Tests screenshots
+![img](./tests-screenshots/screenshot-1.png)
+
+![img](./tests-screenshots/screenshot-2.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,93 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+describe('isPhoneNumber', () => {
+  test('valid phone number should return true', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+  });
+
+  test('another valid phone number should return true', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+  });
+
+  test('invalid phone number should return false', () => {
+    expect(functions.isPhoneNumber('abc')).toBe(false);
+  });
+
+  test('another invalid phone number should return false', () => {
+    expect(functions.isPhoneNumber('*11#notaphonenumber')).toBe(false);
+  });
+});
+
+describe('isEmail', () => {
+  test('valid email should return true', () => {
+    expect(functions.isEmail('test@example.com')).toBe(true);
+  });
+
+  test('another valid email should return true', () => {
+    expect(functions.isEmail('myname2002@hotmail.com')).toBe(true);
+  });
+
+  test('invalid email should return false', () => {
+    expect(functions.isEmail('invalid-email')).toBe(false);
+  });
+
+  test('another invalid email should return false', () => {
+    expect(functions.isEmail('test@example')).toBe(false);
+  });
+});
+
+describe('isStrongPassword', () => {
+  test('valid strong password should return true', () => {
+    expect(functions.isStrongPassword('Password1')).toBe(true);
+  });
+
+  test('another valid strong password should return true', () => {
+    expect(functions.isStrongPassword('SecurePassword1')).toBe(true);
+  });
+
+  test('invalid weak password should return false', () => {
+    expect(functions.isStrongPassword('hey')).toBe(false);
+  });
+
+  test('another invalid weak password should return false', () => {
+    expect(functions.isStrongPassword('1password')).toBe(false);
+  });
+});
+
+describe('isDate', () => {
+  test('valid date should return true', () => {
+    expect(functions.isDate('12/31/2022')).toBe(true);
+  });
+
+  test('another valid date should return true', () => {
+    expect(functions.isDate('01/01/2023')).toBe(true);
+  });
+
+  test('invalid date should return false', () => {
+    expect(functions.isDate('2022-12-31')).toBe(false);
+  });
+
+  test('another invalid date should return false', () => {
+    expect(functions.isDate('July 21st 2002')).toBe(false);
+  });
+});
+
+describe('isHexColor', () => {
+  test('valid hex color should return true', () => {
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+  });
+
+  test('another valid hex color should return true', () => {
+    expect(functions.isHexColor('#00FF00')).toBe(true);
+  });
+
+  test('invalid hex color should return false', () => {
+    expect(functions.isHexColor('ZZZZZZ')).toBe(false);
+  });
+
+  test('another invalid hex color should return false', () => {
+    expect(functions.isHexColor('#12345')).toBe(false);
+  });
+});
